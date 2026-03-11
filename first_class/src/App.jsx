bbfacdc01a78e7954291c0c767a698844096e819
dev-zohaibkhan <zohaib.k.dev@gmail.com>
2026-03-11 21:26:59 +0500

    i i tried to remove some files and add some code

---
+++ first_class/src/App.jsx
@@ -0,0 +1,16 @@
+
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1>Hello I,am <span>Zohaib Khan</span></h1>
+        <p>this is the first react app</p>
+      </div>
+    </>
+  )
+}
+
+export default App
+
