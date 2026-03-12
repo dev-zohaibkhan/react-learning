651fc315fefa2bc18ba1fd90e7655e3bb56229ac
dev-zohaibkhan <zohaib.k.dev@gmail.com>
2026-03-12 19:18:58 +0500

    i fixed some issues

--- first_class/src/App.jsx
+++ first_class/src/App.jsx
@@ -1,13 +1,9 @@
-
-
+import Test from './Testing'
 function App() {
 
   return (
     <>
-      <div>
-        <h1>Hello I,am <span>Zohaib Khan</span></h1>
-        <p>this is the first react app</p>
-      </div>
+      <Test/>
     </>
   )
 }
---
+++ first_class/src/Testing.jsx
@@ -0,0 +1,7 @@
+function Test(){
+   return(
+     <h1>Just for testing!</h1>
+   )
+}
+
+export default Test
