744519a69248495fe541807bf72716fc1e598ca6
dev-zohaibkhan <zohaib.k.dev@gmail.com>
2026-03-13 01:40:00 +0500

    i created simple project in react

---
+++ project-1/src/components/Header.jsx
@@ -0,0 +1,17 @@
+const Header = () => {
+  return (
+    <>
+     <header>
+        <div className="logo">Just!</div>    
+        <ul>
+            <li><a href="">Home</a></li>
+            <li><a href="">About</a></li>
+            <li><a href="">Blogs</a></li>
+            <li><a href="">Contact</a></li>
+        </ul>
+    </header> 
+    </>
+  )
+}
+
+export default Header
